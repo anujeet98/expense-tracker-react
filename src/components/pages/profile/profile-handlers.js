07c01316db4a3bc44c93bc07642680194c90d55e
Anujeet Swain <anujeet98@gmail.com>
2024-03-12 19:29:57 +0530

    profile auto fetch on reload

---
+++ src/components/pages/profile/profile-handlers.js
@@ -0,0 +1,82 @@
+const FIREBASEKEY = process.env.REACT_APP_FIREBASE_KEY;
+
+async function updateProfileBackendHandler(authobj) {
+    try{
+        const res = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${FIREBASEKEY}`,{
+            method: 'POST',
+            body: JSON.stringify(authobj),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        });
+        const resData = await res.json();
+        if(!res.ok){
+            throw new Error(resData.error.message);
+        }
+        return resData;
+    }
+    catch(err){
+        throw err;
+    }
+    finally{
+        // setIsLoading(false);
+    }
+}
+
+const updateProfileHandler = async(e, name, photourl) => {
+    try{
+        e.preventDefault();
+        if(name.trim()==='')
+            return alert('Profile name must be non-empty');
+        if(photourl.trim()==='')
+            return alert('Photo url name must be non-empty');
+        const token  = localStorage.getItem('expensetracker-token');
+
+        if(!token)
+            return alert('user session expired, kindly sign in again');
+
+        const resData = await updateProfileBackendHandler({idToken: token, displayName: name, photoUrl: photourl, deleteAttribute: [], returnSecureToken: true});
+
+        console.log(resData);
+        alert('user profile updated!');
+        // history.push('/home');
+    }
+    catch(err)
+    {
+        console.error(err);
+        alert(err.message);
+    }
+}
+
+const fetchProfileHandler = async() => {
+    try{
+        const token  = localStorage.getItem('expensetracker-token');
+
+        if(!token)
+            throw new Error('user session expired, kindly sign in again');
+
+        const res = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${FIREBASEKEY}`,{
+            method: 'POST',
+            body: JSON.stringify({idToken: token}),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        });
+        const resData = await res.json();
+        if(!res.ok){
+            throw new Error(resData.error.message);
+        }
+        return resData.users[0];
+    }
+    catch(err){
+        throw err;
+    }
+}
+
+
+
+module.exports = {
+    updateProfileBackendHandler,
+    updateProfileHandler,
+    fetchProfileHandler,
+}
--- src/components/pages/profile/profile.jsx
+++ src/components/pages/profile/profile.jsx
@@ -1,62 +1,28 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Button, Container, Form, Row } from "react-bootstrap";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-const FIREBASEKEY = process.env.REACT_APP_FIREBASE_KEY;
+import { updateProfileHandler, fetchProfileHandler } from './profile-handlers';
 
-async function updateProfileBackendHandler(authobj) {
-    try{
-        const res = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${FIREBASEKEY}`,{
-            method: 'POST',
-            body: JSON.stringify(authobj),
-            headers:{
-                'Content-Type': 'application/json'
-            }
-        });
-        const resData = await res.json();
-        if(!res.ok){
-            throw new Error(resData.error.message);
-        }
-        return resData;
-    }
-    catch(err){
-        throw err;
-    }
-    finally{
-        // setIsLoading(false);
-    }
-}
+
 
 const Profile = () => {
     const nameRef = useRef('');
     const photoRef = useRef('');
     const history = useHistory();
 
-    const updateProfileHandler = async(e) => {
-        try{
-            e.preventDefault();
-            if(nameRef.current.value.trim()==='')
-                return alert('Profile name must be non-empty');
-            if(photoRef.current.value.trim()==='')
-                return alert('Photo url name must be non-empty');
-            const token  = localStorage.getItem('expensetracker-token');
-    
-            if(!token)
-                return alert('user session expired, kindly sign in again');
-    
-            const resData = await updateProfileBackendHandler({idToken: token, displayName: nameRef.current.value, photoUrl: photoRef.current.value, deleteAttribute: [], returnSecureToken: true});
-    
-            console.log(resData);
-            alert('user profile updated!');
-            nameRef.current.value='';
-            photoRef.current.value='';
-            // history.push('/home');
-        }
-        catch(err)
-        {
-            console.error(err);
-            alert(err.message);
-        }
-    }
+    useEffect(()=>{
+        (async()=>{
+            try{
+                const resData = await fetchProfileHandler();
+                nameRef.current.value=resData.displayName;
+                photoRef.current.value=resData.photoUrl;
+            }
+            catch(err){
+                console.log(err.message);
+                alert(err.message);
+            }
+        })()
+    },[]);
 
     return (
         <Container fluid className="p-0">
@@ -65,7 +31,7 @@ const Profile = () => {
             </div>
 
             <h1 className="fw-medium mt-3 ps-3" style={{fontSize:'1.5rem'}}>Contact Details</h1>
-            <Form onSubmit={(e)=>updateProfileHandler(e)} className="ps-3">
+            <Form onSubmit={(e)=>updateProfileHandler(e,nameRef.current.value, photoRef.current.value)} className="ps-3">
                 <Row className="d-flex gap-2">
                     <Form.Group className="d-flex col col-12 col-md-5">
                         <Form.Label className="fw-medium w-50">Full Name:</Form.Label>
@@ -77,8 +43,8 @@ const Profile = () => {
                     </Form.Group>
                 </Row>
                 <Row className="d-flex gap-5 mt-3 ps-3">
-                    <Button type="submit" className="col col-3 col-md-1 border-0 " style={{backgroundColor: 'rgb(179, 101, 101)'}}>Update</Button>
-                    <Button className="col col-3 col-md-1" variant="none" style={{border: '2px solid rgb(179, 101, 101)', color: 'rgb(179, 101, 101)'}}>Cancel</Button>
+                    <Button type="submit" className="col col-3 col-lg-1 border-0 " style={{backgroundColor: 'rgb(179, 101, 101)'}}>Update</Button>
+                    <Button className="col col-3 col-lg-1" variant="none" style={{border: '2px solid rgb(179, 101, 101)', color: 'rgb(179, 101, 101)'}}>Cancel</Button>
                 </Row>
             </Form>
         </Container>
